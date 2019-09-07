3778301eb817ae94947a0e6473b5ec849e3e1d8d
joese6 <joese6@gmail.com>
2019-09-07 17:34:32 +0700

    Adding SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Widget titleText=Container(
+    Widget titleText = Container(
       padding: const EdgeInsets.all(10),
       child: Text(
         'Logo HUT Kemerdekaan ke-74 RI',
@@ -20,30 +20,29 @@ class MyApp extends StatelessWidget {
       ),
     );
 
-    Widget detailText=Container(
+    Widget detailText = Container(
       color: Colors.green,
       padding: const EdgeInsets.all(10),
       child: Text(
-        'Kementerian Sekretariat Negara merilis gambar logo untuk peringatan Hari Ulang Tahun Kemerdekaan ke-74 Republik Indonesia dengan tema besar -Menuju Indonesia Unggul-.\n\nMenteri Sekretaris Negara Pratikno telah menandatangani surat No. B 681 /M Sesneg/Set/TU 00 04/06/2019 tentang Penyampaian Tema dan Logo Peringatan Hari Kemerdekaan Tahun 2019 yang disampaikan ke pimpinan lembaga negara dan institusi. Surat diterbitkan pada Senin (24/6/2019).',
-        
+        'Kementerian Sekretariat Negara merilis gambar logo untuk peringatan Hari Ulang Tahun Kemerdekaan ke-74 Republik Indonesia dengan tema besar -Menuju Indonesia Unggul-.\n\nMenteri Sekretaris Negara Pratikno telah menandatangani surat No. B 681 /M Sesneg/Set/TU 00 04/06/2019 tentang Penyampaian Tema dan Logo Peringatan Hari Kemerdekaan Tahun 2019 yang disampaikan ke pimpinan lembaga negara dan institusi. Surat diterbitkan pada Senin (24/6/2019).\n\nDalam surat tersebut, Pratikono menyampaikan bahwa tema dan logo peringata HUT Kemerdekaan ke-74 RI dapat diunduh melaui situs resmi Sekretariat Negara.\n\nMensesneg juga mengimbau kepada pimpinan lemnbaga, institusi pemerintahan, institusi swasta, para kepala daerah, dan pihak terkait lainnya untuk ikut melakukan sosialisasi tema dan logo menyambut HUT RI tersebut.\n\nUntuk menyemarakan HUT Kemerdekaan ke-74 RI, pihaknya meminta masyarakat untuk mengibarkan Bendera Merah Putih serentak di lingkungan instansi mulai 1 Agustus hingga 31 Agustus 2019.\n\nSelain itu, pemerintah mengimbau partisipasi masyarakat dengan memasang umbul-umbul, dekorasi, dan hiasan lainnya yang sejalan dengan tema HUT Kemerdekaan ke-74 RI.',
       ),
     );
 
-    Widget iconSection=Container(
+    Widget iconSection = Container(
       padding: const EdgeInsets.all(20),
       child: Row(
-        mainAxisAlignment:MainAxisAlignment.spaceEvenly,
-        children:[
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
           _buildButtonColumn(Colors.black, Icons.favorite, '550'),
           _buildButtonColumn(Colors.black, Icons.favorite_border, '450'),
-           _buildButtonColumn(Colors.black, Icons.save, '300'),
-            _buildButtonColumn(Colors.black, Icons.delete, '100'),
-             _buildButtonColumn(Colors.black, Icons.add_box, '150'),
+          _buildButtonColumn(Colors.black, Icons.save, '300'),
+          _buildButtonColumn(Colors.black, Icons.delete, '100'),
+          _buildButtonColumn(Colors.black, Icons.add_box, '150'),
         ],
       ),
     );
 
-    Widget iconStar=Container(
+    Widget iconStar = Container(
       padding: const EdgeInsets.all(10),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -57,35 +56,40 @@ class MyApp extends StatelessWidget {
         ],
       ),
     );
+
     return MaterialApp(
       title: 'Tugas 2',
       theme: ThemeData(
         primaryColor: Colors.blueGrey,
       ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.school),
-          title: Text('Tugas 2 - OSG05'),
-        ),
-        
-        body: ListView(
-          children: [
-            Image.asset('images/hutri74.jpg',
-            fit: BoxFit.cover,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            leading: Icon(Icons.school),
+            title: Text('Tugas 2 - OSG05'),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Image.asset(
+                  'images/hutri74.jpg',
+                  fit: BoxFit.cover,
+                ),
+                titleText,
+                detailText,
+                iconStar,
+                iconSection,
+              ],
             ),
-            titleText,
-            detailText,
-            iconStar,
-            iconSection,
-            
-          ],
+          ),
         ),
       ),
     );
   }
 
-Column _buildButtonColumn(Color color, IconData icon, String label) {
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
@@ -106,4 +110,3 @@ Column _buildButtonColumn(Color color, IconData icon, String label) {
     );
   }
 }
-
